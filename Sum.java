e2ccf32d1b930fe8511690ff2b3b6dd78127c18b
Vinod Vatupalli <vinodvatupalli869@gmail.com>
2022-06-11 17:33:35 +0530

    modified

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
 public class Sum {
-    public static void main(String args[]){
-        int a=2;
-        int b=5;
-        int c=a+b;
-        System.out.println(c);
-    }
+  public static void main(String args[]){
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println(c);
+  }  
     
 }
